209a56a14aa42d484786c93616f4aa1aad36f79e
Omar <moussasofa@gmail.com>
2017-11-14 18:57:28 +0100

    ta7dees

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 from collections import defaultdict
 import random
-import cv2
+import cv2 as cv 
 
 
 
@@ -18,7 +18,7 @@ output_vid = sys.argv[3]
 cwd = os.getcwd()
 
 #make new directory
-image_directory = cwd+ '/' + video_file
+image_directory = cwd+ '/frames' 
 
 if not os.path.exists(image_directory):
     os.makedirs(image_directory)
@@ -70,16 +70,33 @@ def polygon_gen(data_frame,color_list):
 	    frame_num = data_frame.ix[index,1]
 	    temp = data_frame.ix[index,3:]
 	    polygon_values = temp.dropna().tolist()
-	    it = iter(polygon_values)
-	    zip(it,it)
+	    
 	    name = data_frame.ix[index,2]
 	    poly = polygon.Polygon(name,frame_num)
 	    poly.set_color(color_list.get(name))
-	    poly.set_new_values(it)
+	    poly.set_new_values(polygon_values)
 	    poly_list.append(poly)
 	    index = index+1
 
 	return poly_list
+ def list_zip(alist):         
+        i =0                                                    
+        y=  []
+    	while i < len(alist):
+    		if i < len(alist)-1:
+    		y.append([alist[i], alist[i+1]])
+    		i += 2
+    	return y
+
+
+for polygons in poly_list:   
+        polygons.set_new_values(list_zip(polygons.get_values()))
+
+#Add image path to frame datatype
+#create plotter in frame 
+#write 
+
+
 
 def frame_gen(polygons):
 	 
--- visualizer/frame.py
+++ visualizer/frame.py
@@ -1,30 +1,47 @@
 import polygon
 import os
 import glob
+import cv2 as cv
+import numpy as np 
 
 
 class Frame(object):
 
  
-    def __init__(self, number,polygons,directory):
+    def __init__(self, number,polygons):
         """Constructor"""
         self._number = number
         self._polygons = polygons
         self._image = None
-        self._directory = directory
+        self._directory = None
+
+
+    
+    def set_directory(self):
+        cwd = os.getcwd()
+        directory = cwd + '/frames'
+        
+
+        return directory
+
  
-    def set_new_image(self, directory):
+    def set_new_image(self):
         """Update your polygon"""
+
+        directory = set_directory()
         os.chdir(directory)
         for file in glob.glob(create_num(number)+'.png'):
 
             self._image= file
 
+            return _image
+
+
 
         
    
     def create_num(number):
-        x = str(number)
+        x = str(number+1)
         leng = len(x)
         y = 0
         while(y<(6-leng)):
@@ -40,4 +57,15 @@ class Frame(object):
         #self._polygons.append(polygon)
 
     def get_polygons(self):
-        return self._polygons    
+        return self._polygons
+
+
+    def plot(polygons,number):
+        img = cv.imread(set_new_image(),1)
+        for poly in polygons.get_polygons():
+            pts = np.array(poly.get_values(), np.int32)
+            pts . pts.reshape(-1,1,2)
+            cv.polylines(img,[pts],True,poly.get_color())
+
+
+
